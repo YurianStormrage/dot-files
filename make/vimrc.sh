41daedb13639db38a7d100aa265981a86bd0bc41
An <pqqqan@foxmail.com>
2024-01-11 17:10:22 +0800

    export variables to .zshenv instead of .zshrc & mkdir for backup/swap/undo files

--- make/vimrc.sh
+++ make/vimrc.sh
@@ -7,5 +7,14 @@ cp -r vim/. ${HOME}
 #     echo 'export MYVIMRC='\''~/.vim/vimrc'\' >> ${ZDOTDIR}/.zshrc
 # fi
 ZDOTDIR=${XDG_CONFIG_HOME:-$HOME/.config}/zsh/
-echo 'export VIMINIT='\''source $MYVIMRC'\' >> ${ZDOTDIR}/.zshrc
-echo 'export MYVIMRC='\''~/.vim/vimrc'\' >> ${ZDOTDIR}/.zshrc
+echo '# +-----+' >> $ZDOTDIR/.zshenv
+echo '# | Vim |' >> $ZDOTDIR/.zshenv
+echo '# +-----+' >> $ZDOTDIR/.zshenv
+echo '' >> $ZDOTDIR/.zshenv
+echo 'export VIMINIT='\''source $MYVIMRC'\' >> ${ZDOTDIR}/.zshenv
+echo 'export MYVIMRC='\''${XDG_CONFIG_HOME:-$HOME/.config}/vim/.vimrc'\' >> ${ZDOTDIR}/.zshenv
+echo '' >> $ZDOTDIR/.zshenv
+
+mkdir -p ${XDG_STATE_HOME:-$HOME/.local/state}/vim/.backup/
+mkdir -p ${XDG_STATE_HOME:-$HOME/.local/state}/vim/.swp/
+mkdir -p ${XDG_STATE_HOME:-$HOME/.local/state}/vim/.undo/
