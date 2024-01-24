8a143b133bf2467eb3e1f46ae04af60d17ddad3b
An <pqqqan@foxmail.com>
2024-01-24 17:24:50 +0800

    use another url for git clone autojump.

--- make/zsh/plugin/autojump.sh
+++ make/zsh/plugin/autojump.sh
@@ -1,1 +1,1 @@
-git clone git://github.com/wting/autojump.git ${XDG_DATA_HOME:-$HOME/.local/share}/autojump
+git clone https://github.com/wting/autojump.git ${XDG_DATA_HOME:-$HOME/.local/share}/autojump
