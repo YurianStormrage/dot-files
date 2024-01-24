6a0d2d1dd8d516d7daed54dcb4c59c354a1ca5a6
An <pqqqan@foxmail.com>
2024-01-24 20:43:49 +0800

    fix: command cp misses option -r

--- make/zsh/zshrc.sh
+++ make/zsh/zshrc.sh
@@ -1,4 +1,4 @@
 ZDOTDIR=${ZDOTDIR:-$HOME/.config/zsh}
 [[ -f $ZDOTDIR/.zshrc ]] && mv $ZDOTDIR/.zshrc $ZDOTDIR/.zshrc.old
 [[ -f $ZDOTDIR/.zshenv ]] && mv $ZDOTDIR/.zshenv $ZDOTDIR/.zshenv.old
-cp zsh/. $HOME
+cp -r zsh/. $HOME
